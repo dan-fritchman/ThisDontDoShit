b4796e2c290270922e897feca2bc5433a5ab18a2
Dan Fritchman <dan@fritch.mn>
2020-05-02 15:58:07 -0700

    Create code.py

---
+++ code.py
@@ -0,0 +1,14 @@
+# This is "code", kind of
+
+// But it doesn't do shit
+
+/* In fact by including a bunch of languages, it dont even run, in any of them. */ 
+
+def do_shit():
+  ''' SEE i told you that wouldnt happen '''
+  ...
+  
+int main() {
+  return 1;
+}
+
